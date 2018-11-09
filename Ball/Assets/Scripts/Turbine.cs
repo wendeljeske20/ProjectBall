26a8c33856dd39e88ae42089fb68b81925eb93ee
wendel <wendelblankjeske@gmail.com>
2018-11-08 23:18:33 -0200

    v0.31

---
+++ Ball/Assets/Scripts/Turbine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turbine : MonoBehaviour {
+
+    public int force;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<Rigidbody>().AddForce(transform.up * force);
+        }
+    }
+}
