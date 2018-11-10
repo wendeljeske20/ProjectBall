6e2f537667090b531e451151a1c760ab35e92950
Alexandre <alexandre.voitikoski@gmail.com>
2018-11-10 19:09:17 -0200

    v0.3.3
    SpinnerSplendi

---
+++ Ball/Assets/Scripts/Spinner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public GameObject father;
+    public GameObject[] platformer;
+    public float rotationX;
+    void Update()
+    {
+        rotationX = GetComponent<Rigidbody>().angularVelocity.x;
+        for (int i = 0; i < platformer.Length; i++)
+        {
+            platformer[i].transform.rotation = father.transform.rotation;
+        }
+        if (GetComponent<Rigidbody>().angularVelocity.x < -0.1)
+        {
+            father.transform.Translate(new Vector3(0, 0, rotationX * 3) * Time.deltaTime);
+        }
+        else if (GetComponent<Rigidbody>().angularVelocity.x > 0.1)
+        {
+            father.transform.Translate(new Vector3(0, 0, rotationX * 3) * Time.deltaTime);
+        }
+    }
+    
+}
