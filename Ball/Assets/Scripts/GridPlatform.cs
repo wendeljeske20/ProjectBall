825608a2a9aa741a8ccc91ff17348ac4d1ff8b6d
Alexandre182 <alexandre.voitikoski@gmail.com>
2018-11-20 08:51:51 -0200

    TestScene

---
+++ Ball/Assets/Scripts/GridPlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPlatform : MonoBehaviour {
+
+    public GameObject arrow;
+    public int timer = 0;
+    public int rotationTime = 0;
+    public float rotationY = 0;
+    bool stop = false;
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (!stop)
+        {
+            timer++;
+        }
+        rotationY = arrow.transform.eulerAngles.y;
+        if(timer >= rotationTime)
+        {
+            timer = 0;
+            arrow.transform.eulerAngles = new Vector3(-90, rotationY + 90, 0);
+            
+        }
+    }
+    private void OnCollisionStay(Collision c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            stop = true;
+            transform.position += -arrow.transform.right / 8;
+            //GetComponent<Rigidbody>().AddForce(-arrow.transform.right / 8);
+        }
+    }
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            stop = false;
+        }
+    }
+}
