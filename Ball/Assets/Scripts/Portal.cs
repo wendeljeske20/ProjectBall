a70d757a039195ed6cf11ed9e75bb947866b58ad
Alexandre <alexandre.voitikoski@gmail.com>
2018-11-19 23:25:15 -0200

    v0.5
    v0.5

---
+++ Ball/Assets/Scripts/Portal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour {
+
+    public string teleportSceneName;
+    public bool startTeleport = false;
+    public int existenceTime = 0;
+	
+    void Update()
+    {
+        if (startTeleport)
+        {
+            existenceTime++;
+            if(existenceTime >= 1000)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(teleportSceneName);
+        }
+    }
+
+    
+}
